058b71b4e5b5b305269a898d32ae9d7f6b114988
mcodevb <getmainul1@gmail.com>
2021-08-04 15:14:40 +0530

    edited the definition of my_f
    some description

--- src/MyPackage.jl
+++ src/MyPackage.jl
@@ -2,11 +2,9 @@ module MyPackage
    using ForwardDiff
    export my_f, my_f_der
 
-    function my_f(x)
-        return 2x
-    end
+   my_f(x)=2x;
 
-    my_f_der=ForwardDiff.derivative(x->my_f(x))
+  my_f_der(x)=ForwardDiff.derivative(x->my_f(x))
 
 
 end
