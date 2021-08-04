ff051dc781d156d02459f25e1c6786ce3e8d8fe9
mcodevb <getmainul1@gmail.com>
2021-08-04 15:19:59 +0530

    Added some comment

--- src/MyPackage.jl
+++ src/MyPackage.jl
@@ -2,9 +2,11 @@ module MyPackage
    using ForwardDiff
    export my_f, my_f_der
 
+   # define your function here
    my_f(x)=2x;
-
-  my_f_der(x)=ForwardDiff.derivative(x->my_f(x))
+   
+   # automated differentiation using ForwardDiff Package.
+   my_f_der(x)=ForwardDiff.derivative(x->my_f(x))
 
 
 end
